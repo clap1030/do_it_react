a217342c9ec35eaa22747032da7eec1f457eb81d
clap1030 <clap1030@gmail.com>
2021-06-16 14:53:03 +0900

    Do it! first Commit

---
+++ src/01/RCC.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class RCC extends Component {
+  render() {
+    return <div>Do it! 리액트프로그래밍 정석 시작</div>;
+  }
+}
+
+export default RCC;
---
+++ src/02/02-1.js
@@ -0,0 +1,6 @@
+//ES6에서 문자열 템플릿, 백틱(`) 사용하기
+
+var str1 = "안녕하세요";
+var str2 = "반값습니다";
+var greeting = `${str1} 처음 뵙겠습니다. ${str2}`;
+console.log(greeting);
